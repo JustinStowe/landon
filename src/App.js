973a215cb7c7c9022710de4d5085a3cdbd9154d4
Justin Stowe <justinstowe12@gmail.com>
2023-06-12 11:31:45 -0600

    Decided to use Context Controller to keep code clean and organized

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./App.css";
 import { Header, Main, Footer } from "./components";
+import { ProvideController } from "./context/useController";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Main />
-        <Footer />
+        <ProvideController>
+          <Header />
+          <Main />
+          <Footer />
+        </ProvideController>
       </div>
     );
   }
---
+++ src/context/useController.js
@@ -0,0 +1,72 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+const ControllerContext = createContext({});
+
+export function ProvideController({ children }) {
+  const provider = useHook();
+  return (
+    <ControllerContext.Provider value={provider}>
+      {children}
+    </ControllerContext.Provider>
+  );
+}
+
+export const useController = () => {
+  return useContext(ControllerContext);
+};
+
+function useHook() {
+  /*
+   *useState
+   */
+  const [galleryImages, setGalleryImages] = useState([]);
+  const [menuLinksData, setMenuLinksData] = useState([]);
+  const [accessibilitiesData, setAccessibilitiesData] = useState([]);
+  const [servicesData, setServicesData] = useState([]);
+  /*
+   *functions
+   */
+
+  const loadGalleryImages = async () => {
+    const response = await fetch(
+      "https://0kbeg1cv46.execute-api.us-east-1.amazonaws.com/Production/accessibilities"
+    );
+    let jsonData = await response.json();
+    setGalleryImages(jsonData);
+    return galleryImages;
+  };
+  const loadMenuLinksData = async () => {
+    const response = await fetch(
+      "https://0kbeg1cv46.execute-api.us-east-1.amazonaws.com/Production/menuLinks"
+    );
+    let jsonData = await response.json();
+    setMenuLinksData(jsonData);
+    return menuLinksData;
+  };
+  const loadAccessibilitiesData = async () => {
+    const response = await fetch(
+      "https://0kbeg1cv46.execute-api.us-east-1.amazonaws.com/Production/accessibilities"
+    );
+    let jsonData = await response.json();
+    setAccessibilitiesData(jsonData);
+    return accessibilitiesData;
+  };
+  const loadServicesData = async () => {
+    const response = await fetch(
+      "https://0kbeg1cv46.execute-api.us-east-1.amazonaws.com/Production/services"
+    );
+    let jsonData = await response.json();
+    setServicesData(jsonData);
+    return servicesData;
+  };
+  return {
+    loadGalleryImages,
+    galleryImages,
+    loadMenuLinksData,
+    menuLinksData,
+    loadAccessibilitiesData,
+    accessibilitiesData,
+    loadServicesData,
+    servicesData,
+  };
+}
